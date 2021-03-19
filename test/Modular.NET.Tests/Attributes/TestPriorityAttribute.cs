f28e914a8526514acd6fdbd962bae976e4cf1b9d
KelvinGo <iam@kelvingo.com>
2021-03-20 02:45:21 +0800

    Add orderer and attribute in base test library

---
+++ test/Modular.NET.Tests/Attributes/TestPriorityAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Modular.NET.Tests.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TestPriorityAttribute : Attribute
+    {
+        #region Fields, Properties and Indexers
+
+        public int Priority { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public TestPriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        #endregion
+    }
+}
