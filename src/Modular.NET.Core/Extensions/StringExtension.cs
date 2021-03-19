efcd525ebe3b48d915d483ac498bf1a0ba63c694
KelvinGo <iam@kelvingo.com>
2021-03-20 01:51:58 +0800

    Added StringExtension

---
+++ src/Modular.NET.Core/Extensions/StringExtension.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+// ReSharper disable once CheckNamespace
+public static class StringExtension
+{
+    #region Static Methods
+
+    public static string GenerateRandomString(this string stringValue,
+        int length,
+        bool numeric = false,
+        bool lowercase = false,
+        bool uppercase = false,
+        string additionalCharacters = null)
+    {
+        var ret = new StringBuilder();
+        var validCharacters = new StringBuilder();
+
+        if (lowercase)
+        {
+            validCharacters.Append("abcdefghijklmnopqrstuvwxyz");
+        }
+
+        if (uppercase)
+        {
+            validCharacters.Append("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        }
+
+        if (numeric)
+        {
+            validCharacters.Append("0123456789");
+        }
+
+        if (!string.IsNullOrEmpty(additionalCharacters))
+        {
+            validCharacters.Append(additionalCharacters);
+        }
+
+        if (validCharacters.Length == 0)
+        {
+            validCharacters.Append("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+        }
+
+        for (var i = 0; i < length; i++)
+        {
+            ret.Append(validCharacters[RandomNumberGenerator.GetInt32(0, validCharacters.Length - 1)]);
+        }
+
+        return ret.ToString();
+    }
+
+    public static bool CheckLuhn(this string stringValue)
+    {
+        if (string.IsNullOrEmpty(stringValue) || stringValue.Length < 2)
+        {
+            return false;
+        }
+
+        if (!stringValue.All(char.IsDigit))
+        {
+            return false;
+        }
+
+        var reverse = stringValue.Select(x => Convert.ToInt32(x) - 48)
+            .Reverse()
+            .ToList();
+        var counter = 0;
+
+        for (var i = 1; i < reverse.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                counter += reverse[i];
+            }
+            else
+            {
+                var x2 = reverse[i] * 2;
+                while (x2.ToString()
+                    .Length != 1)
+                {
+                    x2 = x2.ToString()
+                        .Select(x => Convert.ToInt32(x) - 48)
+                        .Sum();
+                }
+
+                counter += x2;
+            }
+        }
+
+        var checkDigit = 10 - counter % 10;
+
+        return reverse[0] == checkDigit;
+    }
+
+    public static string Pad(this string stringValue,
+        int length,
+        char paddingChar = ' ',
+        bool paddingLeft = true)
+    {
+        var ret = stringValue;
+
+        if (string.IsNullOrEmpty(ret))
+        {
+            ret = "";
+        }
+
+        if (ret.Length < length)
+        {
+            ret = paddingLeft
+                ? ret.PadLeft(length, paddingChar)
+                : ret.PadRight(length, paddingChar);
+        }
+
+        return ret;
+    }
+
+    #endregion
+}
---
+++ test/Modular.NET.Core.Tests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Modular.NET.Tests.Utilities;
+using Xunit;
+
+namespace Modular.NET.Core.Tests.Extensions
+{
+    public class StringExtensionTests
+    {
+        #region Static Fields and Constants
+
+        private const int _TestRandomRound = 10;
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void StringExtension_GenerateRandomString_NoPrefer()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = "".GenerateRandomString(Generator.RandomInt());
+                Assert.Matches(@"^[0-9a-zA-Z]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_GenerateRandomString_NumberOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = "".GenerateRandomString(Generator.RandomInt(), true);
+                Assert.Matches(@"^[0-9]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_GenerateRandomString_LowercaseOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = "".GenerateRandomString(Generator.RandomInt(), lowercase: true);
+                Assert.Matches(@"^[a-z]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_GenerateRandomString_UppercaseOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = "".GenerateRandomString(Generator.RandomInt(), uppercase: true);
+                Assert.Matches(@"^[A-Z]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_GenerateRandomString_WithAdditionalCharacter()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = "".GenerateRandomString(Generator.RandomInt(), true, true, true, " -_");
+                Assert.Matches(@"^[0-9a-zA-Z_\-\s]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_CheckLuhn()
+        {
+            Assert.True("79927398713".CheckLuhn());
+            Assert.False("79927398719".CheckLuhn());
+            Assert.False("".CheckLuhn());
+            Assert.False("a79927398713".CheckLuhn());
+        }
+
+        [Fact]
+        public void StringExtension_Pad()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomLength = Generator.RandomInt();
+                var randomValue = Generator.RandomString();
+                var pad = randomValue.Pad(randomLength);
+                Assert.Equal(Math.Max(randomLength, randomValue.Length), pad.Length);
+                if (randomLength > randomValue.Length)
+                {
+                    Assert.StartsWith(" ", pad);
+                }
+            }
+        }
+
+        [Fact]
+        public void StringExtension_Pad_Right()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomLength = Generator.RandomInt();
+                var randomValue = Generator.RandomString();
+                var pad = randomValue.Pad(randomLength, paddingLeft: false);
+                Assert.Equal(Math.Max(randomLength, randomValue.Length), pad.Length);
+                if (randomLength > randomValue.Length)
+                {
+                    Assert.EndsWith(" ", pad);
+                }
+            }
+        }
+
+        [Fact]
+        public void StringExtension_Pad_EmptyString()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomLength = Generator.RandomInt();
+                Assert.Equal(randomLength, "".Pad(randomLength)
+                    .Length);
+            }
+        }
+
+        [Fact]
+        public void StringExtension_Pad_StringLengthLongerThanParamLength()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = Generator.RandomString();
+                var randomLength = Generator.RandomInt(1, randomValue.Length);
+                Assert.Equal(randomValue.Length, randomValue.Pad(randomLength)
+                    .Length);
+            }
+        }
+
+        #endregion
+    }
+}
